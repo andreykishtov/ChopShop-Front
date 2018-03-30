e6cd6f4e23f9fa4e810c026751e0ac11d257063e
Andrey Kishtov <andreykishtov@gmail.com>
2018-03-30 17:52:15 +0300

    eslint2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App/App';
-import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import App from './Components/App/App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 import configureStore from './Store/Store';
 
 const store = configureStore();
