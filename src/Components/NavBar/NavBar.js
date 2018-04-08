80cd005ec19a22e2b6f0f51d9889c824bc1b540b
Andrey Kishtov <andreykishtov@gmail.com>
2018-04-08 13:30:01 +0300

    created a responsive navbar #19

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -8,6 +8,7 @@ const Wrapper = styled.div`
   flex-direction: row;
   justify-content: space-between;
   background: grey;
+
 `;
 
 const Container = styled.div`
@@ -15,38 +16,72 @@ width: 60%;
 display: flex;
   flex-direction: row;
   justify-content: flex-end;
+
+@media (max-width: 768px) {
+  width: 80%;
+  justify-content: space-around;
+  }
+
 `;
 
 const Logo = styled.div`
   width: 20%;
+  padding: 2%;
   font-size: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 768px) {
+  width: 20%;
+  }
+
+@media (max-width: 544px) {
+  font-size:1.5rem;
+  }
+
 `;
 
 const Login = styled.div`
   width: 20%;
+  padding: 2%;
   font-size: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
+
+@media (max-width: 544px) {
+    font-size:1.5rem;
+  }
+
 `;
 
 const LogOut = styled.div`
   width: 20%;
+  padding: 2%;
   font-size: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  
+  @media (max-width: 544px) {
+    font-size:1.5rem;
+  }
+
 `;
 
 const Cart = styled.div`
   width: 20%;
+  padding: 2%;
   font-size: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
+
+@media (max-width: 544px) {
+    font-size:1.5rem;
+  }
+
 `;
 
 const NavBar = () => (
